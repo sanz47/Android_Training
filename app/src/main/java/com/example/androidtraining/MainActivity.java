5ffa21234242782e2e943b4458888e8dad285b35
Syem Aziz <syemaziz@iut-dhaka.edu>
2022-06-02 22:41:13 +0600

    commited 2nd

--- app/src/main/java/com/example/androidtraining/MainActivity.java
+++ app/src/main/java/com/example/androidtraining/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 txtMsg.setText("Good Bye");
+                Toast.makeText(MainActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
             }
         });
     }
