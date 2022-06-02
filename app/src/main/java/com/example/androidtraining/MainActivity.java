a42534e8e1602252bc445ebd79d83228da60b84f
Syem Aziz <syemaziz@iut-dhaka.edu>
2022-06-02 22:27:41 +0600

    First Commit

---
+++ app/src/main/java/com/example/androidtraining/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.androidtraining;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button click=findViewById(R.id.clickBtn);
+        TextView txtMsg=findViewById(R.id.txtMsg);
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtMsg.setText("Good Bye");
+            }
+        });
+    }
+}
