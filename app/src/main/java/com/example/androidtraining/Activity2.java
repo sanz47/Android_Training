299c3f3efe06ebf3511f9e9872e02ee89e4df7d9
Syem Aziz <syemaziz@iut-dhaka.edu>
2022-06-04 17:11:32 +0600

    Slight Change in Checkbox

--- app/src/main/java/com/example/androidtraining/Activity2.java
+++ app/src/main/java/com/example/androidtraining/Activity2.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.Toast;
 
 public class Activity2 extends AppCompatActivity {
@@ -15,6 +17,9 @@ public class Activity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
         Button goback=findViewById(R.id.go_back);
+        CheckBox chkbx1=findViewById(R.id.checkBox);
+        CheckBox chkbx2=findViewById(R.id.checkBox2);
+        CheckBox chkbx3=findViewById(R.id.checkBox3);
         goback.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -22,6 +27,17 @@ public class Activity2 extends AppCompatActivity {
                 Intent goingBack=new Intent(Activity2.this,MainActivity.class);
                 startActivity(goingBack);
 
+            }
+        });
+        chkbx1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    Toast.makeText(Activity2.this, "Dr.Strange Done", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(Activity2.this, "You Need to Watch Dr.Strange", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
